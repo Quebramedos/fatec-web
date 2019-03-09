2ed0837a0c667afbd3e4cb8dfd45935c851dc170
Kelvin augusto <kelvin.zuzor@gmail.com>
2019-03-09 10:37:20 -0300

    Add files via upload

--- teste.js
+++ teste.js
@@ -1,10 +1,11 @@
-let express = require('express');
-let router = express.Router();
-let controller = require('../controllers/teste');
-
-/* GET home page. */
-router.get('/', controller().teste);
-
-
-
-module.exports = router;
+module.exports =  function(){
+    let controller = {};//objeto vazio
+    controller.teste = function(req, res){
+        let json = {
+            nome:'Darth Vader',
+            idade: 33
+        }
+        res.send(json);
+    }
+    return controller;
+}
--- veiculo.js
+++ veiculo.js
@@ -1,22 +1,102 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/veiculo');
+const Veiculo = require('../models/Veiculo.')(/=construtor =/);
 
-/* post */
-router.post('/', controller.novo);
+const controller = {};// objeto vazio
 
-/*get&/*/
-router.get('/', controller.listar);
+controller.novo = function (req, res) {
+    // os dados para sere gravados estao no req.body
 
-/*obterum*/
-router.get('/:id', controller.obterUm);
+    Veiculo.create(req.body).then(
+        //calback se der certo
+        function () {
+            res.send(null);
+            //falha error 201
+            res.sendStatus(201).end();
+        },
+        function (erro) {
+            console.error(erro);
+            //htt 500 erro interno de servidor
+            res.sendStatus(500).end();
+        }
+    );
+    //res.send('Dados gravados!');
 
-/*obterum*/
-router.patch('/', controller.atualizar);
 
-//excluir
-router.delete('/:id', controller.excluir);
+};
 
+controller.listar = function (req, res) {
+    Veiculo.find().exec().then(
+        //callback do bem
+        function (veiculos) {//todos os veikculos do vetor 
+            //REtorna o vetor encontrado
+            res.json(veiculos).end();
+        },
+        function (erro) {
+            console.error(erro)
+            res.sendStatus(500).end();
+        }
+    );
 
-module.exports = router;
+};
 
+controller.obterUm = function (req, res) {
+    const id = req.params.id;
+
+    Veiculo.findById(id).exec().then(
+
+        //callback do bem
+        function (veiculo) {
+            if (veiculo) {
+                res.json(veiculo).end();
+            } else {
+                res.sendStatus(404).end();
+            }
+
+        },
+        function (erro) {
+            console.error(erro);
+            res.sendStatus(500).end();
+        }
+    )
+};
+
+controller.atualizar = function (req, res) {
+    //capturar o id dentro do corpo da requisiçao
+    const id = req.body._id;
+    //encontra o objeto indentificado pelo id e substitui o seu conteudo por req.body       
+    Veiculo.findByIdAndUpdate({ _id: id }, req.body).exec().then(
+        //callback do bem
+        function (veiculo) {
+            if (veiculo) {
+                res.sendStatus(204).end();
+            } else {
+                res.sendStatus(404).end();
+            }
+        },
+        //callback do mal
+        function (erro) {
+            console.error(erro)
+            res.sendStatus(500).end();
+        }
+    );
+};
+
+controller.excluir = function (req, res) {
+    const id = req.params.id;
+    //capturamos o id a partir da url do veiculo
+    Veiculo.findByIdAndDelete({ _id: id }).exec().then(
+        //callback do bem
+        function (veiculo) {
+            if (veiculo) {
+                res.sendStatus(204).end();
+            } else {
+                res.sendStatus(404).end();
+            }
+        },
+        function (erro) {
+            //callback do mal
+            console.error(erro);
+            res.sendStatus(500).end();
+        }
+    );
+}
+module.exports = controller;
